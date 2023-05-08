a403fa2212e8a52af9a21565ea461bdf4d4ec1ba
Isrothy <isrothy@gmail.com>
2023-05-08 21:46:46 +0800

    8 May 2023

---
+++ .config/nvim/lua/plugins/async.lua
@@ -0,0 +1,8 @@
+return {
+	"skywind3000/asyncrun.vim",
+	cmd = {
+		"AsyncRun",
+		"AsyncReset",
+		"AsyncStop",
+	},
+}
--- .config/nvim/lua/plugins/markdown-integration.lua
+++ .config/nvim/lua/plugins/markdown-integration.lua
@@ -4,14 +4,47 @@ return {
 		build = "cd app && npm install",
 		cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
 		ft = { "markdown" },
+		enabled = false,
 		config = function()
-			vim.g.mkdp_markdown_css = "/Users/jiangjoshua/.config/nvim/style/markdown.css"
-			vim.g.mkdp_highlight_css = "/Users/jiangjoshua/.config/nvim/style/highlight.css"
+			local home = os.getenv("HOME")
+			vim.g.mkdp_markdown_css = home .. "/.config/nvim/style/markdown.css"
+			vim.g.mkdp_highlight_css = home .. "/.config/nvim/style/highlight.css"
 			vim.g.mkdp_theme = "dark"
+			vim.g.mkdp_auto_close = 0
 		end,
 	},
 	{
+		"toppair/peek.nvim",
+		build = "deno task --quiet build:fast",
+		ft = { "markdown" },
+		config = function()
+			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+			require("peek").setup({
+				auto_load = false, -- whether to automatically load preview when
+				-- entering another markdown buffer
+				close_on_bdelete = true, -- close preview window on buffer delete
 
+				syntax = true, -- enable syntax highlighting, affects performance
+
+				theme = "dark", -- 'dark' or 'light'
+
+				update_on_change = true,
+
+				app = "browser", -- 'webview', 'browser', string or a table of strings
+				-- explained below
+
+				filetype = { "markdown" }, -- list of filetypes to recognize as markdown
+
+				-- relevant if update_on_change is true
+				throttle_at = 200000, -- start throttling when file exceeds this
+				-- amount of bytes in size
+				throttle_time = "auto", -- minimum amount of time in milliseconds
+				-- that has to pass before starting new render
+			})
+		end,
+	},
+	{
 		"gaoDean/autolist.nvim",
 		ft = { "markdown" },
 		enabled = true,
